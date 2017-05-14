6627ac993d4ab92f023eaf714779c772aca78106
vickyliin <r05922128@ntu.edu.tw>
2017-05-14 17:09:43 +0800

    add lyrics encoder, move config

--- model.py
+++ model.py
@@ -3,6 +3,7 @@
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
+from config import *
 
 use_cuda = torch.cuda.is_available()
 
@@ -35,25 +36,10 @@ def load(filename):
 
   return sd
 
-# max music length
-L = 300
-# music channel number
-Ci = 5
-# embedding size
-E = 50
-# CNN kernel size
-K = 10
-# CNN output channel
-Co = 200
-
-# encoded vector size
-M = 100
-
-
 class MusicEncoder(nn.Module):
-  L, Ci, E, K, Co, M = L, Ci, E, K, Co, M
-  # Dropout rate
-  dp = .5
+  L, Ci, E, K, Co, M = music.L, music.Ci, music.E, music.K, music.Co, M
+  dp = music.dp
+  assert L >= K, 'MusicEncoder: Seq length should >= kernel size'
   @param(inp = ['LongTensor', (300, Ci, L)])
   def __init__(self, vs):
     # vs: vocabulary size
@@ -67,7 +53,7 @@ class MusicEncoder(nn.Module):
 
   def forward(self, inp):
     # inp: torch tensor, N x Ci x L
-    # out: torch tensor, N x M
+    # out: torch tensor variable, N x M
     assert type(inp).__name__.endswith('Tensor')
     inp = self.inp.resize_(inp.size()).copy_(inp)
     inp = Variable(inp)
@@ -86,31 +72,28 @@ class MusicEncoder(nn.Module):
     return out
 
 class MusicDecoder(nn.Module):
-  L, Ci, E, K, Co, M = L, Ci, E, K, Co, M
-  # Dropout rate
-  dp = .5
-
-  @param(inp = ['FloatTensor', (300, M)])
+  L, Ci, E, K, Co, M = music.L, music.Ci, music.E, music.K, music.Co, M
+  dp = music.dp
+  assert L >= K, 'MusicDecoder: Seq length should >= kernel size'
+  @param()
   def __init__(self, vs):
     # vs: vocabulary size
     super().__init__()
     self.linear = nn.Linear(self.M, self.Co)
-    self.unpool = nn.Linear(1, self.L)
+    self.unpool = nn.Linear(1, self.L+self.K-1)
     self.unconv = nn.Conv1d(self.Co, self.Ci*self.E, self.K)
     self.dropout = nn.Dropout(self.dp, inplace=True)
     self.pointer = nn.Linear(self.E, vs)
 
 
   def forward(self, inp):
-    # inp: torch tensor, N x M
-    # out: torch tensor, N*Ci*L x V
-    assert type(inp).__name__.endswith('Tensor')
-    inp = self.inp.resize_(inp.size()).copy_(inp)
-    inp = Variable(inp)
+    # inp: torch tensor variable, N x M
+    # out: torch tensor variable, N*Ci*L x V
+    assert type(inp).__name__ == 'Variable'
 
     hid = self.linear(inp)                        # N x Co
-    hid = self.unpool(hid.view(-1,1))             # N*Co x L
-    hid = hid.view(-1, self.Co, self.L)           # N x Co x L
+    hid = self.unpool(hid.view(-1,1))             # N*Co x L+K-1
+    hid = hid.view(-1, self.Co, self.L+self.K-1)  # N x Co x L+K-1
     self.dropout(hid)
 
     emb = self.unconv(hid)                        # N x Ci*E x L
@@ -125,21 +108,94 @@ class MusicDecoder(nn.Module):
     return out
 
 
-class Translator(nn.Module):
-  def __init__(self):
+class LyricsEncoder(nn.Module):
+  L, E, K, Co, M = lyrics.L, lyrics.E, lyrics.K, lyrics.Co, M
+  dp = lyrics.dp
+  assert L >= K, 'LyricsEncoder: Seq length should >= kernel size'
+  @param(inp = ['LongTensor', (300, L)])
+  def __init__(self, vs):
+    # vs: vocabulary size
     super().__init__()
+    self.embed = nn.Embedding(vs, self.E)
+    self.dropout = nn.Dropout(self.dp, inplace=True)
+    self.conv = nn.Conv2d(1, self.Co, (self.K, self.E))
+    self.pool = nn.MaxPool1d(self.L, ceil_mode=True)
+    self.linear = nn.Linear(self.Co, self.M)
+
 
   def forward(self, inp):
-    pass
+    # inp: torch tensor, N x L
+    # out: torch tensor variable, N x M
+    assert type(inp).__name__.endswith('Tensor')
+    inp = self.inp.resize_(inp.size()).copy_(inp)
+    inp = Variable(inp)
+
+    emb = self.embed(inp).unsqueeze(1)            # N x 1 x L x E
+    self.dropout(emb)
+
+    hid = self.conv(emb).squeeze(-1)              # N x Co x L-
+    hid = self.pool(hid).squeeze(-1)              # N x Co
+    self.dropout(hid)
+
+    out = self.linear(hid)                        # N x M
+    self.dropout(out)
+
+    return out
+
+class Translator(nn.Module):
+  @param()
+  def __init__(self, encoder, decoder):
+    # encoder: lyrics encoder / music encoder
+    # decoder: music decoder
+    super().__init__()
+    self.encoder = encoder
+    self.decoder = decoder
+
+  def forward(self, inp):
+    # inp: torch tensor, N x L / N x Ci x L
+    # out: torch tensor variable, N*Ci*L x Vm
+    assert type(inp).__name__.endswith('Tensor')
+
+    enc = self.encoder(inp)
+    dec = self.decoder(enc)
+    return dec
 
   def translate(self, inp):
     pass
 
 if __name__ == '__main__':
-  me = MusicEncoder(10)
-  print(me)
-  print(me(me.inp))
+  vsL, vsM = 10, 5
+  n = 3
+  inpL = torch.floor( torch.rand(n, lyrics.L) * vsL )
+  inpM = torch.floor( torch.rand(n, music.Ci, music.L) * vsM )
 
-  md = MusicDecoder(10)
+  le = LyricsEncoder(vsL)
+  print(le)
+  outLe = le(inpL)
+  assert outLe.size(0) == n
+  assert outLe.size(1) == M
+
+  me = MusicEncoder(vsM)
+  print(me)
+  outMe = me(inpM)
+  assert outMe.size(0) == n
+  assert outMe.size(1) == M
+
+  md = MusicDecoder(vsM)
   print(md)
-  print(md(md.inp))
+  outMd = md(outMe)
+  assert outMd.size(0) == n*music.Ci*music.L
+  assert outMd.size(1) == vsM
+
+  tr = Translator(le, md)
+  print('Translator: ', tr)
+  outTr = tr(inpL)
+  assert outTr.size(0) == n*music.Ci*music.L
+  assert outTr.size(1) == vsM
+
+
+  ae = Translator(me, md)
+  print('Autoencoder: ', ae)
+  outAe = ae(inpM)
+  assert outAe.size(0) == n*music.Ci*music.L
+  assert outAe.size(1) == vsM
