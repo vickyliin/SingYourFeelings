76803458654901b0f8d47d0a85da4806e5db7858
danielko841212 <daniel841212ko@gmail.com>
2017-05-14 17:42:18 +0800

    快好了~~
    快好了~~大致好了 不過後面還有一些問題還沒確定可不可以用

--- crawler/midi_download.py
+++ crawler/midi_download.py
@@ -1,2 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import midi
+result=requests.get('http://sql.jaes.ntpc.edu.tw/javaroom/midi/alas/Ch/ch.htm')
+page=result.text
+doc=BeautifulSoup(page,"html.parser")
+pagelist=doc.find_all('td')
+pagelinks=[page.find('a') for page in pagelist]
+
+pagelinks=[page.get('href') for page in pagelist]
+for pagelink in pagelinks:
+    titlelinks=midi.titlelink(pagelink)
+    for songurl in titlelinks:
+        try:
+            midi.songtext(songurl)
+        except:
+            print(Exception)
