3c695488c9e26390fd1b07210893d88d6e8625e1
vickyliin <r05922128@ntu.edu.tw>
2017-06-14 09:23:58 +0000

    reconnection on close

--- gui/main.js
+++ gui/main.js
@@ -1,9 +1,17 @@
 var host = "54.254.210.178",
-    port = "5678";
-var ws = new WebSocket("ws://" + host + ":" + port);
-ws.onmessage = function (event) {
+    port = "5678",
+    ws = null;
+
+onmessage = function(event){
   MIDIjs.play("/midifiles/" + event.data);
 };
+start = function(){
+  ws = new WebSocket("ws://" + host + ":" + port);
+  ws.onmessage = onmessage;
+  ws.onclose = start;
+};
+
+start();
 $('#send-lyrics').click(function(){
   ws.send($('#lyrics').val());
 });
--- server.py
+++ server.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
 import os
 import uuid
-from subprocess import Popen
+from subprocess import Popen, DEVNULL
+import logging
 
 import asyncio
 import websockets
 
 import model
 
+logging.getLogger('websockets.protocol').setLevel(logging.ERROR)
+logging.getLogger('asyncio').setLevel(logging.ERROR)
+logging.getLogger('jseg.jieba').setLevel(logging.ERROR)
+
 model.use_cuda = False
-outpath = 'midifiles'
+outpath = 'gui/midifiles'
 para = 'model/test.para'
 
-cleaner = Popen('watch -n 3600 find midifiles/ -mmin +60 -delete'.split())
-
-async def hello(websocket, path):
+cmd = 'watch -n 300 find {}/ -mmin +5 -delete'.format(outpath)
+cleaner = Popen(cmd.split(), stdout=DEVNULL, stderr=DEVNULL)
+async def midigen(websocket, path):
     while True:
         text = await websocket.recv()
         print("< {}".format(text))
         midi = tr.translate(text)
         filename = '%s/%s.mid' % (outpath, uuid.uuid1())
+        os.makedirs(outpath, exist_ok=True)
         with open(filename, 'wb') as f:
             midi.writeFile(f)
 
@@ -31,9 +37,7 @@ tr = model.Translator()
 sd = model.load(para)
 tr.load_state_dict(sd)
 
-os.makedirs(outpath, exist_ok=True)
-
-start_server = websockets.serve(hello, '0.0.0.0', 5678)
+start_server = websockets.serve(midigen, '0.0.0.0', 5678, timeout=86400)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
